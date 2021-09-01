1ddb50fb5ab02d0b953fb6694fab20654b04e1d4
Russell Mercer <russellshome@gmail.com>
2021-09-01 14:55:10 +1200

    Add pest  & weed javascript and css

--- src/PestHubPageController.php
+++ src/PestHubPageController.php
@@ -7,12 +7,11 @@ use SilverStripe\View\Requirements;
 class PestHubPageController extends \PageController 
 {
     public function getPestContent() {
-        Requirements::javascript(
-            'gdmedia/pestsandweeds: client/pwscript.js'
-        );
+        Requirements::javascript('https://pw.gurudigital.nz/WebAPI/PanelScript?organisationId=4');
+        Requirements::css('https://pw.gurudigital.nz/theme/styles/webapi.css');
+        Requirements::javascript('gdmedia/pestsandweeds: client/pwscript.js');
         return PestHub::getPestContent();
     }
-
 }
 
 ?>
