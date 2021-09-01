a40b896eb7349211e1fc3677368e4bd32d9665e7
Russell Mercer <russellshome@gmail.com>
2021-09-02 08:28:48 +1200

    Fire script only when required elements are on the page

--- client/pwscript.js
+++ client/pwscript.js
@@ -1,21 +1,31 @@
 window.pwurlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
     if (results == null){
-       return null;
+        return null;
     }
     else {
-       return decodeURI(results[1]) || 0;
+        return decodeURI(results[1]) || 0;
     }
 }
-if (window.pwurlParam('sort') !== null || window.pwurlParam('tags') !== null || window.pwurlParam('classification') !== null|| window.pwurlParam('pwid') !== null) {
-    document.getElementById("pw-temp").style = "display:none;";
-    document.getElementById("pw-panel").style = "";
-} else {
-    document.addEventListener("click", function(e) {
-        document.getElementById("pw-temp").style = "display:none;";
-        document.getElementById("pw-panel").style = "";
-        if (e.target.classList.contains("pw-results-link")) {
-            document.getElementById("pw-panel").scrollIntoView();
-        }
-    });
-}
+/*
+The list of pests/weeds output by calling the API is displayed in pw-temp
+This must remain displayed to be read by search engine bots.
+When displaying an individual pest/weed or after "View Results" is clicked 
+the list in pw-temp is hidden and the data supplied from the pest&weeds site displayed  
+*/
+document.addEventListener("DOMContentLoaded", function() {
+    if (document.getElementById("pw-temp") && document.getElementById("pw-panel")) {
+        if (window.pwurlParam('sort') !== null || window.pwurlParam('tags') !== null || window.pwurlParam('classification') !== null|| window.pwurlParam('pwid') !== null) {
+            document.getElementById("pw-temp").style = "display:none;";
+            document.getElementById("pw-panel").style = "";
+        } else {
+            document.addEventListener("click", function(e) {
+                document.getElementById("pw-temp").style = "display:none;";
+                document.getElementById("pw-panel").style = "";
+                if (e.target.classList.contains("pw-results-link")) {
+                    document.getElementById("pw-panel").scrollIntoView();
+                }
+            });
+        }  
+    }
+});
